6b5e6acf0c2ed3b86ba15261112972ee4257986f
Roudy Ghosn <roudy.ghosn@capital-banking.com>
2019-05-16 19:24:51 +0300

    Adding Dependencies

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ app.post('/webhook', function (req, res) {
   var size = req.body.queryResult.parameters['size'];
   var flavour = req.body.queryResult.parameters['flavour'];
 
-  // var atmAndBranches = getAtmAndBranches();
-  // console.log('AtmAndBranches Results ' + atmAndBranches);
+  var atmAndBranches = getAtmAndBranches();
+  console.log('AtmAndBranches Results ' + atmAndBranches);
 
   let response = " ";
   let responseObj = {
@@ -42,39 +42,39 @@ app.listen(process.env.PORT || 3000);
 
 /* Calling WSO2 Web APIS */
 
-// function getAtmAndBranches() {
-//   var https = require('https');
-//   var success = function(data) {console.log('Success');};
-//   var dataString = JSON.stringify({'lng': 'en'});
-//   console.log(`REST API Start! data = ${dataString} and success = ${success}`);
+function getAtmAndBranches() {
+  var https = require('https');
+  var success = function(data) {console.log('Success');};
+  var dataString = JSON.stringify({'lng': 'en'});
+  console.log(`REST API Start! data = ${dataString} and success = ${success}`);
 
-//   var options = {
-//     host: 'cmbaas.capital-banking.com',
-//     path: '/cn/public/atmbranches',
-//     method: 'POST',
-//     headers: {'Content-Type': 'application/json',
-//               'Content-Length': dataString.length}
-//   };
+  var options = {
+    host: 'cmbaas.capital-banking.com',
+    path: '/cn/public/atmbranches',
+    method: 'POST',
+    headers: {'Content-Type': 'application/json',
+              'Content-Length': dataString.length}
+  };
 
-//   var req = https.request(options, function(res) {
-//     console.log(`HTTPS request just started !`);
-//     res.setEncoding('utf-8');
-//     var responseString = '';
+  var req = https.request(options, function(res) {
+    console.log(`HTTPS request just started !`);
+    res.setEncoding('utf-8');
+    var responseString = '';
 
-//     res.on('data', function(data) {
-//       responseString += data;
-//       console.log(`responseString on DATA !`);
-//     });
+    res.on('data', function(data) {
+      responseString += data;
+      console.log(`responseString on DATA !`);
+    });
 
-//     res.on('end', function() {
-//       console.log(`responseString on END !` + responseString);
-//       var responseObject = JSON.parse(responseString);
-//       success(responseObject);
-//     });
-//   });
+    res.on('end', function() {
+      console.log(`responseString on END !` + responseString);
+      var responseObject = JSON.parse(responseString);
+      success(responseObject);
+    });
+  });
 
-//   req.write(dataString);
-//   req.end();
-//   console.log(`REST API End !`);
-//   return responseObject;
-// }
+  req.write(dataString);
+  req.end();
+  console.log(`REST API End !`);
+  return responseObject;
+}
