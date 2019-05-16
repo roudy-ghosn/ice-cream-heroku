c0fbc213f800269786545ca4315f82abca4e01fd
Roudy Ghosn <roudy.ghosn@capital-banking.com>
2019-05-16 18:00:36 +0300

    New Changes

--- index.js
+++ index.js
@@ -1,35 +1,70 @@
-var https = require('https');
-var success = function(data) {
-  console.log('Success');
-};
-var dataString = JSON.stringify({'lng': 'en'});
-console.log(`REST API Start! data = ${dataString}`);
 
-var options = {
-  host: 'cmbaas.capital-banking.com',
-  path: '/cn/public/atmbranches',
-  method: 'POST',
-  headers: {'Content-Type': 'application/json',
-            'Content-Length': dataString.length}
-};
+/* WebHook */
 
-var req = https.request(options, function(res) {
-  console.log(`HTTPS request just started !`);
-  res.setEncoding('utf-8');
-  var responseString = '';
+app.host('/webhook', function(req, res){
+  console.log('Received a POST request');
+  
+  if(!req.body)
+    return res.sendStatus(400);
+  res.setHeader('Content-Type', 'application/json');
+  
+  console.log('Heres the post request from DialogFlow');
+  console.log(req.body);
+  console.log('Got Size: ' + req.body.queryResult.parameters['size'] + ' And Flavour: ' + req.body.queryResult.parameters['flavour'] + ' from DialogFlow');
 
-  res.on('data', function(data) {
-    responseString += data;
-    console.log(`responseString on DATA !`);
+  var size = req.body.queryResult.parameters['size'];
+  var flavour = req.body.queryResult.parameters['flavour'];
+
+  var atmAndBranches = getAtmAndBranches();
+  console.log('AtmAndBranches Results ' + atmAndBranches);
+
+  let response = " ";
+  let responseObj = {
+                      "fullfillmentText" : response,
+                      "fullfillmentMessages" : [{"text": {"text": [atmAndBranches]}}],
+                      "source": ""
+                    }
+
+  console.log('Heres the response to DialogFlow');
+  console.log(responseObj);
+  return res.json(responseObj);
+})
+
+/* Calling WSO2 Web APIS */
+
+function getAtmAndBranches() {
+  var https = require('https');
+  var success = function(data) {console.log('Success');};
+  var dataString = JSON.stringify({'lng': 'en'});
+  console.log(`REST API Start! data = ${dataString} and success = ${success}`);
+
+  var options = {
+    host: 'cmbaas.capital-banking.com',
+    path: '/cn/public/atmbranches',
+    method: 'POST',
+    headers: {'Content-Type': 'application/json',
+              'Content-Length': dataString.length}
+  };
+
+  var req = https.request(options, function(res) {
+    console.log(`HTTPS request just started !`);
+    res.setEncoding('utf-8');
+    var responseString = '';
+
+    res.on('data', function(data) {
+      responseString += data;
+      console.log(`responseString on DATA !`);
+    });
+
+    res.on('end', function() {
+      console.log(`responseString on END !` + responseString);
+      var responseObject = JSON.parse(responseString);
+      success(responseObject);
+    });
   });
 
-  res.on('end', function() {
-    console.log(`responseString on END !` + responseString);
-    var responseObject = JSON.parse(responseString);
-    success(responseObject);
-  });
-});
-
-req.write(dataString);
-req.end();
-console.log(`REST API End !`);
+  req.write(dataString);
+  req.end();
+  console.log(`REST API End !`);
+  return responseObject;
+}
