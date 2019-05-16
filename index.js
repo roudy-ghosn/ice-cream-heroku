1447119dd1470b30ddf847155a46efacc1716f39
Roudy Ghosn <roudy.ghosn@capital-banking.com>
2019-05-16 19:15:48 +0300

    Adding Dependencies

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.post('/webhook', function (req, res) {
 
   let response = " ";
   let responseObj = {
-                      "fullfillmentText" : response,
+                      "fullfillmentText" : "Hello Abou l Roudz, hay anne Heruko !",
                       // "fullfillmentMessages" : [{"text": {"text": [atmAndBranches]}}],
                       "source": ""
                     }
