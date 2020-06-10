9abac079e558d9883319f2c0f22374cdc9ca1f4f
Roudy Ghosn <roudy.ghosn@capital-banking.com>
2020-06-10 11:54:15 +0300

    MobileAgent Modifications

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 var path = require("path");
 var server = require('https').createServer(app);
 var io = require('socket.io')(server);
+var array = [];
 
 /* Webhook */
 
@@ -19,37 +20,40 @@ app.post('/webhook', function (req, res) {
   if(!req.body)
     return res.sendStatus(400); 
   res.setHeader('Content-Type', 'application/json');
-  
-  if (req.body.queryResult.intent['displayName'] === "getUserNavigationRequest") {
-    var page = req.body.queryResult.parameters['Pages'];
-    var fulfillmentMessage = 'Got Page: ' + page + ' from DialogFlow';
-    res.json({ fulfillmentText: fulfillmentMessage,
-               fulfillmentMessages: [{"text": {"text": [fulfillmentMessage]}}],
-               outputContexts: [{ "name": "projects/ice-cream-helper-8226e/agent/sessions/13c7b519-028d-5517-bfab-f10585e781b3/contexts/go-to-action",
-                                  "lifespanCount": 1,
-                                  "parameters": {
-                                    "page": page
-                                }}],
-               source: 'webhook-heroku'
-            });
-  } else if (req.body.queryResult.intent['displayName'] === "getIceCreamOrder") {
-    var size = req.body.queryResult.parameters['size'];
-    var flavour = req.body.queryResult.parameters['flavours'];
-    var fulfillmentMessage = 'Got Size: ' + size + ' And Flavour: ' + flavour + ' from DialogFlow';
-    res.json({ fulfillmentText: fulfillmentMessage,
-               fulfillmentMessages: [{"text": {"text": [fulfillmentMessage]}}],
-               outputContexts: [{ "name": "projects/ice-cream-helper-8226e/agent/sessions/13c7b519-028d-5517-bfab-f10585e781b3/contexts/order",
-                                  "lifespanCount": 1,
-                                  "parameters": {
-                                    "size": size,
-                                    "flavour": flavour
-                                }}],
-               source: 'webhook-heroku'
-            });
+
+  if (req.body.queryResult.intent['displayName'] === "balance.check") {
+    var param              = '';
+    var fulfillmentMessage = '';
+    var balanceType        = req.body.queryResult.parameters['balance-type'];
+    var digits             = req.body.queryResult.parameters['number'];
+
+    if (balanceType.hasOwnProperty("global")) {
+      param = req.body.queryResult.parameters['balance-type'].['global'];
+    } else if (balanceType.hasOwnProperty("account-type")) {
+      param = req.body.queryResult.parameters['balance-type'].['account-type'];
+    } else if (balanceType.hasOwnProperty("card-type")) {
+      param = req.body.queryResult.parameters['balance-type'].['card-type'];
+    }
+
+    if (balanceType.hasOwnProperty("global")) {
+      var fulfillmentMessage = 'Your current global balance is equivalent to: 850 USD';
+    } else if (digits != null) {      
+      var fulfillmentMessage = 'Your current ' + param + ' balance is equivalent to: 150 USD';
+    } else {
+      var fulfillmentMessage = 'Please provide the last 4 digits of the requested ' + param;
+    }
+    
+    array.push({ fulfillmentText: fulfillmentMessage,
+                 fulfillmentMessages: [{"text": {"text": [fulfillmentMessage]}}],
+                 source: 'webhook-heroku'
+              });
   }
   
   // var atmAndBranches = getAtmAndBranches();
   // console.log('AtmAndBranches Results ' + atmAndBranches);
+
+  console.log(array);
+  res.json(array);
 })
 
 app.listen((process.env.PORT || 8000), function() {
